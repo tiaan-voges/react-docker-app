03ab971c5abeb6984506569645e8e2aff094ef1b
Tiaab <tiaan.voges@gmail.com>
2026-01-10 19:21:18 +0200

    test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ export default function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-slate-900">
       <h1 className="text-4xl font-bold text-emerald-400">
-        Tailwind is working 🚀
+        Tailwind is working 🚀 and testing new
       </h1>
     </div>
   );
